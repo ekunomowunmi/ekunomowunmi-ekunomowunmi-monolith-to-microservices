8629c0702aa75086d079ac5cfdafb4e46edc84a6
Omowunmi Rachael Ekun <52955661+ekunomowunmi@users.noreply.github.com>
2022-07-11 12:56:15 +0100

    Add files via upload

---
+++ set_env.sh
@@ -0,0 +1,11 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=monolithdevwunmi
+export POSTGRES_PASSWORD=monolithdevwunmi
+export POSTGRES_HOST=monolithdevwunmi.c66qnpg5kesl.us-east-1.rds.amazonaws.com
+export POSTGRES_DB=postgres
+export AWS_BUCKET=monolith-138202428557-dev
+export AWS_REGION=us-east-1
+export AWS_PROFILE=default
+export JWT_SECRET=testing
+export URL=http://localhost:8100
